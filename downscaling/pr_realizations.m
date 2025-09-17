ca5dddcd588017419f783dc5e064eaa9da96f967
PR Guaita <guaita.pierluigi@gmail.com>
2025-09-17 16:02:43 +0200

    fixed pr realization inversion

--- downscaling/pr_realizations.m
+++ downscaling/pr_realizations.m
@@ -13,7 +13,7 @@ for i_mth = 1:12
         [name_var '_PCR_models_mth=' int2str(i_mth) '-' int2str(i_mth) suffix '.mat']),'O_t_local')
     idx_mth = i_mth:12:size(EOhat,2);
     Ot = O_t_local.O_t;
-    Ohat(:,idx_mth) = exp(u_mat(:,idx_mth)-sigma2/2) .* (EOhat(:,idx_mth)+Ot);
+    Ohat(:,idx_mth) = exp(u_mat(:,idx_mth)-sigma2/2) .* (EOhat(:,idx_mth)+Ot) - Ot;
 end
 
 % Step 2: Hybrid Zero-Inflated Gamma post-processing (per station, per month) ----
@@ -32,7 +32,6 @@ for i_mth = 1:12
 
             % Step 2b: define wet-day mask
             pos_vals = row(row>0);
-            p = numel(pos_vals)/numel(row);  % wet-day probability
 
             if numel(pos_vals) >= 10
                 % fit Gamma to positives
@@ -52,8 +51,6 @@ for i_mth = 1:12
             q99 = prctile(pos_vals, 99);
             extreme_mask = row > q99 & row>20;
             row(extreme_mask) = gamrnd(k,theta,[1,sum(extreme_mask)]);
-
-            % Step 2d: optional – keep wet-day values below q99 as-is
         end
 
         Ohat_proc(i,idx_mth) = row;
